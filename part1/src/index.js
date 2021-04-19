cade55de1fb8db28333e971cd880c83fa7114840
Gonzalo Andrade <gonzaloandrade@molx-jsalvador.falabella.com>
2021-04-19 19:37:42 -0400

    Ejercicio 1.6 Paso 1

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
+import Unicafe from './components/unicafe/Unicafe';
 
 ReactDOM.render(
-    <App />,
+    <Unicafe />,
   document.getElementById('root')
 );
 
