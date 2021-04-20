93bf22fbea485a90d809e9567f16f01ef0e7bac1
Gonzalo Andrade <gonzaloandrade@molx-jsalvador.falabella.com>
2021-04-19 21:38:17 -0400

    Paso 5

--- part1/src/components/unicafe/Statistics.js
+++ part1/src/components/unicafe/Statistics.js
@@ -1,34 +1,16 @@
 const Statistics = (props) => {
 
-    const { good, neutral, bad, allClicks, getAverage, getPorcentual } = props;
-
-    if (allClicks.length === 0) {
-        return (
-            <>
-                <h1>statistics</h1>
-                <p>No feedback given</p>
-            </>
-
-        )
-
-    } else {
-
-        return (
-            <>
-                <h1>statistics</h1>
-                <div>
-                    good {good} <br />
-                    neutral {neutral}  <br />
-                    bad {bad} <br />
-                    all {allClicks.length}<br />
-                    average {getAverage(allClicks)} <br />
-                    positive {getPorcentual(good, allClicks)} <br />
-                </div>
+    const { value, text} = props;
 
 
-            </>
-        )
-    }
+    return (
+        <>
+          <p> {text} <span> { value } </span> </p>
+
+
+        </>
+    )
+
 
 }
 
--- part1/src/components/unicafe/Unicafe.js
+++ part1/src/components/unicafe/Unicafe.js
@@ -3,51 +3,51 @@ import Statistics from "./Statistics";
 
 const Unicafe = () => {
 
-    const [ good, setGood ] = useState(0);
-    const [ neutral, setNeutral ] = useState(0);
-    const [ bad, setBad ] = useState(0);
-    const [ allClicks, setAllClicks] = useState([]);
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
+    const [allClicks, setAllClicks] = useState([]);
 
-    const Button = ( props ) => {
+    const Button = (props) => {
         //console.log(props);
-        const { text , onclick } = props;
-        return(
+        const { text, onclick } = props;
+        return (
             <button onClick={onclick}>
-                { text }
+                { text}
             </button>
         )
     }
 
-    const handleGood = () => { 
+    const handleGood = () => {
         setGood(good + 1);
         setAllClicks(allClicks.concat(1));
     };
-    
+
     const handleNeutral = () => {
         setNeutral(neutral + 1);
         setAllClicks(allClicks.concat(0));
     };
-    
+
     const handleBad = () => {
         setBad(bad + 1);
         setAllClicks(allClicks.concat(-1));
     };
 
-    const getAverage = ( clicks ) => {
+    const getAverage = (clicks) => {
         let total = 0;
 
-        if ( clicks.length > 0) {
-            clicks.map( val => total += val );
+        if (clicks.length > 0) {
+            clicks.map(val => total += val);
             total = total / clicks.length;
         }
 
         return total;
     }
 
-    const getPorcentual = ( vote, clicks ) => {
-        
+    const getPorcentual = (vote, clicks) => {
+
         let porcentual = ".00%";
-        
+
         if (clicks.length === 0) {
             return 0 + porcentual;
         } else {
@@ -55,26 +55,29 @@ const Unicafe = () => {
         }
     }
 
-    return(
+    return (
         <>
 
             <h1>give feedback</h1>
 
-            <Button text="good" onclick={handleGood}/>
-            <Button text="neutral" onclick={handleNeutral}/>
-            <Button text="bad" onclick={handleBad}/>
+            <Button text="good" onclick={handleGood} />
+            <Button text="neutral" onclick={handleNeutral} />
+            <Button text="bad" onclick={handleBad} />
 
-           <Statistics 
-                good={ good }
-                neutral= { neutral }
-                bad={ bad }
-                allClicks = { allClicks }
-                getAverage = { getAverage }
-                getPorcentual = { getPorcentual }
-           />
+            <h1>statistics</h1>
 
-           
-            
+            {
+                allClicks.length === 0 
+                ?   <p> No feedback given</p>
+                :   <>
+                    <Statistics value={ good} text="godd" />
+                    <Statistics value={ neutral } text="neutral" /> 
+                    <Statistics value={ bad } text="bad" /> 
+                    <Statistics value={ allClicks.length } text="all" /> 
+                    <Statistics value={ getAverage(allClicks) } text="average" /> 
+                    <Statistics value={ getPorcentual(good, allClicks) } text="positive" /> 
+                    </>
+            }   
         </>
     )
 
