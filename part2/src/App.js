f9153f4e6ba6344ff795362c1d9aa0ea8d2db7a1
Gonzalo Andrade <gonzaloandrade@mondcmartinez.falabella.com>
2021-04-20 21:50:42 -0400

    First commit

---
+++ part2/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Note from './components/Note';
+
+const App = ( { notes } ) => {
+
+  return(
+
+    <div>
+    <h1>Notes</h1>
+    <ul>
+      {
+        notes.map( n => <Note key={ n.id } note={ n.content } />)
+      }
+    </ul>
+  </div>
+
+
+  )
+}
+
+
+
+export default App;
