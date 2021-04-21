21a97417b392e9f3507c5bb3fe08367618911b4f
Gonzalo Andrade <gonzaloandrade@mondcmartinez.falabella.com>
2021-04-20 23:37:13 -0400

    Step 9 random()

--- part2/src/App.js
+++ part2/src/App.js
@@ -4,27 +4,52 @@ import Course from './components/Course';
 
 const App = ( { notes } ) => {
 
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ];
+
+  const course = Math.floor((Math.random() * courses.length) + 0);;
 
   return(
 
@@ -36,7 +61,7 @@ const App = ( { notes } ) => {
   //     }
   //   </ul>
   // </div>
-      <Course course={ course } /> 
+      <Course course={ courses.[course] } /> 
   )
 }
 
