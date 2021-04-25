a48529d264b88d25b7d2a3db4537971e656015c8
Gonzalo Andrade <gonzaloandrade@ncsenc01.falabella.com>
2021-04-25 00:48:02 -0400

    Exercises 2.11-2.14

--- countries/src/components/CountryInfo.js
+++ countries/src/components/CountryInfo.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 const CountryInfo = ( {country} ) => {
+
+    const api_key = process.env.REACT_APP_API_KEY;
+
+    const [ weather, setWeather] = useState({
+        current: {
+            temperature: 0,
+            wind_speed: '',
+            wind_dir: '',
+            weather_icons: [{}]
+        }
+    });
+    
+    useEffect( () => {
+        axios.get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${country.capital}`)
+            .then( response => {
+                setWeather(response.data);
+                console.log(weather);
+            }).catch( error => console.log(error) );
+    }, []);
 
     return (
         <div>
@@ -17,6 +38,15 @@ const CountryInfo = ( {country} ) => {
             </ul>
 
             <img src={ country.flag } alt={ country.name } width="100px" height="100px" />
+
+            <h2>Weather in { country.capital } </h2>
+
+            <p><b>temperature: { weather.current.temperature } Celcius </b></p>
+
+            <img src={weather.current.weather_icons[0]} />
+
+            <p><b>wind:</b> { weather.current.wind_speed } direction={ weather.current.wind_dir } </p>
+
 
         </div>
     )
